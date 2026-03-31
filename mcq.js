bebe036f6018dcb80b07fa5554c4732b4805fe92
sudarshanreddy22 <sudarshanece22@gmail.com>
2026-03-31 17:58:07 +0530

    Add files via upload

---
+++ mcq.js
@@ -0,0 +1,91 @@
+const mcqQuestions = [
+  {
+    question: `A convex lens of focal length 15 cm is forming a real image. If the size of image is same as the size of object, the position of object and image respectively will be:`,
+    options: {
+      A: "−15 cm and −15 cm from lens",
+      B: "−15 cm and +15 cm from lens",
+      C: "−30 cm and +30 cm from lens",
+      D: "−30 cm and −30 cm from lens"
+    },
+    answer: "C",
+    set: "31/1/1",
+    year: "2026"
+  },
+  {
+    question: `From the following options choose the one which gives negative magnification greater than one:`,
+    options: {
+      A: "Concave mirror; Nature of image - real",
+      B: "Concave mirror; Nature of image - virtual",
+      C: "Convex mirror; Nature of image - real",
+      D: "Convex mirror; Nature of image - virtual"
+    },
+    answer: "A",
+    set: "31/2/1",
+    year: "2026"
+  },
+  {
+    question: `Rays from the sun converge at a point 25 cm behind convex lens. The distance at which an object be placed in front of the lens to get a virtual image, is:`,
+    options: {
+      A: "20 cm",
+      B: "40 cm",
+      C: "50 cm",
+      D: "More than 50 cm"
+    },
+    answer: "D",
+    set: "31/5/1",
+    year: "2026"
+  },
+  {
+    question: `A 5 cm long rod is placed vertically in front of a concave mirror. Its 5 cm image is formed at 20 cm in front of the mirror. The focal length of the mirror is:`,
+    options: {
+      A: "−20 cm",
+      B: "−10 cm",
+      C: "−5 cm",
+      D: "+20 cm"
+    },
+    answer: "B",
+    set: "31/S/1",
+    year: "2025"
+  },
+  {
+    question: `An optical device which always produces images with magnification m = +1 is:`,
+    options: {
+      A: "Plane mirror",
+      B: "Convex mirror",
+      C: "Concave mirror",
+      D: "Convex lens"
+    },
+    answer: "A",
+    set: "31/S/1",
+    year: "2025"
+  },
+  {
+    question: `Mirror X is used in solar furnace and Mirror Y is used as a rear-view mirror.<br>
+(i) Image by X is real and at focus<br>
+(ii) Image by Y is virtual and diminished<br>
+(iii) Image by X is virtual and erect<br>
+(iv) Image by Y is real at focus<br>
+Which statements are correct?`,
+    options: {
+      A: "(i) and (ii)",
+      B: "(ii) and (iii)",
+      C: "(iii) and (iv)",
+      D: "(i) and (iv)"
+    },
+    answer: "A",
+    set: "31/1/1",
+    year: "2025"
+  },
+  {
+    question: `If the absolute refractive indices of two media X and Y are 6/5 and 4/3 respectively, then the refractive index of Y with respect to X will be :`,
+    options: {
+      A: "10/9",
+      B: "9/10",
+      C: "9/8",
+      D: "8/9"
+    },
+    answer: "A",
+    set: "31/2/1",
+    year: "2025"
+  }
+];
