baa2b2f8de4e187b2f9bbc06f6595627c83230a4
sudarshanreddy22 <sudarshanece22@gmail.com>
2026-04-08 14:08:54 +0530

    Add files via upload

--- grade10/light/pyqs/msq.js
+++ grade10/light/pyqs/msq.js
@@ -1,5 +1,4 @@
-/* MCQ — Light: Reflection & Refraction | Grade 10
-   Format: SET X – YEAR at top of each question card */
+/* MCQ — Light: Reflection & Refraction | Grade 10 Format: SET X – YEAR at top of each question card */
 window.mcqData = [
   {
     question: "A convex lens of focal length 15 cm is forming a real image. If the size of image is same as the size of object, the position of object and image respectively will be:",
@@ -11,28 +10,33 @@ window.mcqData = [
     options:{A:"Concave mirror; Nature of image - real",B:"Concave mirror; Nature of image - virtual",C:"Convex mirror; Nature of image - real",D:"Convex mirror; Nature of image - virtual"},
     answer:"A", set:"31/2/1", year:"2026"
   },
-{
+  {
     question:"Rays from the sun converge at a point 25 cm behind convex lens. The distance at which an object be placed in front of the lens to get a virtual image, is:",
     options:{A:"20 cm",B:"40 cm",C:"50 cm",D:"More than 50 cm"},
     answer:"D", set:"31/5/1", year:"2026"
   },
   {
-    question:"A 5 cm long rod is placed vertically in front of a concave mirror. If its 5 cm long image is formed at 20 cm in front of the mirror, the focal length of the mirror is:",
-    options:{A:"-20 cm",B:"-10 cm",C:"-5 cm",D:"20 cm"},
+    question:"1.	A 5 cm long rod is placed vertically in front of a concave mirror. If its 5 cm long image is formed at 20 cm in front of the mirror, the focal length of the mirror is :",
+    options:{A:"− 20 cm",B:"− 10 cm",C:"− 5 cm",D:"20 cm"}, 
     answer:"B", set:"31/S/1", year:"2025"
   },
   {
-    question:"An optical device which always produces images of m = + 1 is:",
-    options:{A:"Plane mirror",B:"Convex mirror ",C:"Concave mirror",D:"Convex lens"},
+    question:"An object is placed at a distance of 30 cm from the pole of a concave mirror. If its real and inverted image is formed at 60 cm in front of the mirror, the focal length of the mirror is:",
+    options:{A:"– 15 cm",B:"– 20 cm",C:"+ 20 cm",D:"+ 15 cm"},
+    answer:"B", set:"31/2/1", year:"2025"
+  },
+  {
+    question:"An optical device which always produces images of m = +1 is:",
+    options:{A:"Plane mirror",B:"Convex mirror",C:"Concave mirror",D:"Convex lens"},
     answer:"A", set:"31/S/1", year:"2025"
   },
   {
-    question:"Mirror 'X' is used to concentrate sunlight in solar furnace and Mirror 'Y' is fitted on the side of the vehicle to see the traffic behind the driver. Which of the following statements are true for the two mirrors?<br>
+    question:"Mirror 'X' is used to concentrate sunlight in solar furnace and Mirror 'Y' is fitted on the side of the vehicle to see the traffic behind the driver. Which of the following statements are true for the two mirrors?
    (i) The image formed by mirror 'X' is real, diminished and at its focus.<br>
    (ii) The image formed by mirror 'Y' is virtual, diminished and erect.<br>
    (iii) The image formed by mirror 'X' is virtual, diminished and erect.<br>
-   (iv) The image formed by mirror 'Y' is real, diminished and at its focus.",
-    options:{A:"(i) and (ii)",B:"(ii) and (iii) ",C:"(iii) and (iv)",D:"(i) and (iv)s"},
+   (iv) The image formed by mirror 'Y' is real, diminished and at its focus."
+      options:{A:"(i) and (ii)",B:"(ii) and (iii)",C:"(iii) and (iv)",D:"(i) and (iv)"},
     answer:"A", set:"31/1/1", year:"2025"
   },
   {
@@ -40,20 +44,149 @@ window.mcqData = [
     options:{A:"10/9",B:"9/10",C:"9/8",D:"8/9"},
     answer:"A", set:"31/2/1", year:"2025"
   },
-   
   {
-    question:"An object is placed at a distance of 30 cm from the pole of a concave mirror. If its real and inverted image is formed at 60 cm in front of the mirror, the focal length of the mirror is:",
-    options:{A:"– 15 cm",B:"– 20 cm",C:"+ 20 cm",D:"+ 15 cm"},
-    answer:"B", set:"31/2/1", year:"2025"
+    question: "An object is placed at a distance of 30 cm from the pole of a concave mirror. If its real and inverted image is formed at 60 cm in front of the mirror, the focal length of the mirror is :",
+    options: { A: "– 15 cm", B: "– 20 cm", C: "+ 20 cm", D: "+ 15 cm" },
+    answer: "B", set: "31/2/1", year: "2025"
   },
   {
-    question:"An optical device which always produces images with magnification m = +1 is:",
-    options:{A:"Plane mirror",B:"Convex mirror",C:"Concave mirror",D:"Convex lens"},
-    answer:"A", set:"31/S/1", year:"2025"
+    question: "Absolute refractive index of water and glass is 4/3 and 3/2 respectively. If the speed of light in glass is 2x10^8 m/s, the speed of light in water is :",
+    options: { A: "9/4 x 10^8 m/s", B: "8/3 x 10^8 m/s", C: "16/9 x 10^8 m/s", D: "9/8 x 10^8 m/s" },
+    answer: "A", set: "31/3/1", year: "2025"
   },
-
-
-
-
-   
+  {
+    question: "A candle flame is placed in front of the reflecting surface of a convex mirror of focal length f. If the distance of the flame from the pole of the mirror is ‘f’, its image is formed :",
+    options: { A: "at infinite distance from the mirror", B: "behind the mirror at the principal focus", C: "behind the mirror at a distance 2f", D: "behind the mirror at a distance f/2" },
+    answer: "D", set: "31/3/3", year: "2025"
+  },
+  {
+    question: "To get an image of magnification –1 on a screen using a lens of focal length 20 cm, the object distance must be :",
+    options: { A: "Less than 20 cm", B: "30 cm", C: "40 cm", D: "80 cm" },
+    answer: "C", set: "31/4/1", year: "2025"
+  },
+  {
+    question: "An optical device ‘X’ is placed obliquely in the path of a narrow parallel beam of light. If the emergent beam gets displaced laterally, the device ‘X’ is :",
+    options: { A: "plane mirror", B: "convex lens", C: "glass slab", D: "glass prism" },
+    answer: "C", set: "31/4/1", year: "2025"
+  },
+  {
+    question: "In order to obtain large images of the teeth of patients, the dentist holds the concave mirror in such a manner that the teeth are positioned:",
+    options: { A: "at the focus of mirror.", B: "between pole and focus of the mirror.", C: "between focus and centre of curvature of the mirror.", D: "at the centre of curvature of the mirror." },
+    answer: "B", set: "31/5/1", year: "2025"
+  },
+  {
+    question: "An object is placed at a distance of 30 cm from the reflecting surface of a concave mirror of radius of curvature 40 cm. The image formed is:",
+    options: { A: "Virtual and magnified", B: "Virtual and diminished", C: "Real and magnified", D: "Real and diminished" },
+    answer: "C", set: "31(B)", year: "2025"
+  },
+  {
+    question: "The image formed by a concave mirror of focal length 50 cm is real and of magnification – 1. In this case the distance between the object from its own image is:",
+    options: { A: "50 cm", B: "100 cm", C: "200 cm", D: "Zero" },
+    answer: "D", set: "", year: "2024C"
+  },
+  {
+    question: "At what distance from a convex lens should an object be placed to get an image of the same size as that of the object on a screen?",
+    options: { A: "Beyond twice the focal length of the lens.", B: "At the principal focus of the lens.", C: "At twice the focal length of the lens.", D: "Between the optical centre of the lens and its principal focus." },
+    answer: "C", set: "", year: "2024"
+  },
+  {
+    question: "The speed of light in vacuum is 3 x 10⁸ m/s. If the speed of light in a medium is 2.25 x 10⁸ m/s, the absolute refractive index of the medium is :",
+    options: { A: "7/6", B: "5/4", C: "4/3", D: "3/2" },
+    answer: "C", set: "", year: "2024"
+  },
+  {
+    question: "Absolute refractive index of glass and water is 3/2 and 4/3 respectively. If the speed of light in glass is 2x10⁸ m/s, the speed of light in water is:",
+    options: { A: "9/4 x 10⁸ m/s", B: "5/2 x 10⁸ m/s", C: "7/3 x 10⁸ m/s", D: "16/9 x 10⁸ m/s" },
+    answer: "A", set: "", year: "2024"
+  },
+  {
+    question: "How will the image formed by a convex lens be affected, if the upper half of the lens is wrapped with a black paper?",
+    options: { A: "The size of the image formed will be one-half of the size of the image due to complete lens.", B: "The image of upper half of the object will not be formed.", C: "The brightness of the image will reduce.", D: "The lower half of the inverted image will not be formed" },
+    answer: "C", set: "", year: "2024"
+  },
+  {
+    question: "A ray of light is incident on a plane mirror at an angle of incidence of 15º. The deviation in the ray produced by the mirror is:",
+    options: { A: "30º", B: "60º", C: "120º", D: "150º" },
+    answer: "D", set: "", year: "2024"
+  },
+  {
+    question: "For an object, a convex mirror always forms an image which is :",
+    options: { A: "Virtual, erect and magnified", B: "Virtual, erect and diminished", C: "Virtual, inverted and diminished", D: "Real, erect and diminished" },
+    answer: "B", set: "", year: "2024"
+  },
+  {
+    question: "When an object is placed beyond 2F of a convex lens, the nature of the image formed is",
+    options: { A: "Real, inverted and diminished", B: "Real, erect and magnified", C: "Virtual, erect and magnified", D: "Real, inverted and magnified" },
+    answer: "A", set: "", year: "2023C"
+  },
+  {
+    question: "A student wants to obtain an erect image of an object using a concave mirror of 10 cm focal length. What will be the distance of the object from mirror?",
+    options: { A: "Less than 10 cm", B: "10 cm", C: "between 10 cm and 20 cm", D: "more than 20 cm" },
+    answer: "A", set: "", year: "2023"
+  },
+  {
+    question: "An object is placed in front of a convex mirror at infinity. According to the New Cartesian Sign Convention, the sign of the focal length and the sign of image distance in this case are respectively",
+    options: { A: "+, -", B: "-, +", C: "-, -", D: "+, +" },
+    answer: "D", set: "", year: "2023"
+  },
+  {
+    question: "To obtain a magnification of + 2 with a concave mirror of radius of curvature 60 cm the object distance must be",
+    options: { A: "- 90 cm", B: "- 45 cm", C: "-30 cm", D: "- 15 cm" },
+    answer: "D", set: "", year: "2023"
+  },
+  {
+    question: "In torch lights and head lights of vehicles, the bulb is placed",
+    options: { A: "between the pole and the focus of the reflector.", B: "very near to the focus of the reflector.", C: "between the focus and centre of curvature of the reflector", D: "at the centre of curvature of the reflector." },
+    answer: "B", set: "", year: "2023"
+  },
+  {
+    question: "A ray of light starting from air passes through medium A of refractive index 1.50, enters medium B of refractive index 1.33 and finally enters medium C of refractive index 2.42. If this ray emerges out in air from C, then for which of the following pairs of media the bending of light is least?",
+    options: { A: "air-A", B: "A-B", C: "B-C", D: "C-air" },
+    answer: "B", set: "", year: "2021"
+  },
+  {
+    question: "If a lens can converge the sun rays at a point 20 cm away from its optical centre, the power of this lens is",
+    options: { A: "+ 2D", B: "– 2D", C: "+ 5D", D: "– 5D" },
+    answer: "C", set: "", year: "2021"
+  },
+  {
+    question: "The radius of curvature of a converging mirror is 30 cm. At what distance from the mirror should an object be placed so as to obtain a virtual image?",
+    options: { A: "Infinity", B: "30 cm", C: "Between 15 cm and 30 cm", D: "Between 0 cm and 15 cm" },
+    answer: "D", set: "", year: "2021"
+  },
+  {
+    question: "A converging lens forms a three times magnified image of an object, which can be take on a screen. If the focal length of the lens is 30 cm, then the distance of the object from the lens is",
+    options: { A: "- 55 cm", B: "- 50 cm", C: "- 45 cm", D: "- 40 cm" },
+    answer: "D", set: "", year: "2021"
+  },
+  {
+    question: "An object of height 4 cm is kept at a distance of 30 cm from the pole of a diverging mirror. If the focal length of the mirror is 10 cm, the height of the image formed is",
+    options: { A: "+ 3.0 cm", B: "+ 2.5 cm", C: "+ 1.0 cm", D: "+ 0.75 cm" },
+    answer: "C", set: "", year: "2021"
+  },
+  {
+    question: "In which of the following is a concave mirror used?",
+    options: { A: "A solar cooker", B: "A rear view mirror in vehicles", C: "A safety mirror in shopping malls", D: "In viewing full size image of distant tall buildings." },
+    answer: "A", set: "", year: "2021"
+  },
+  {
+    question: "The refractive index of medium A is 1.5 and that of medium B is 1.33. If the speed of light in air is 3 x 10⁸ m/s, what is the speed of light in medium A and B respectively?",
+    options: { A: "2 x 10⁸ m/s and 1.33 x 10⁸ m/s", B: "1.33 x 10⁸ m/s and 2 x 10⁸ m/s", C: "2.25 x 10⁸ m/s and 2 x 10⁸ m/s", D: "2 x 10⁸ m/s and 2.25 x 10⁸ m/s" },
+    answer: "A", set: "", year: "2021"
+  },
+  {
+    question: "A real image is formed by the light rays after reflection or refraction when they :<br>(A) actually meet or intersect with each other.<br>(B) actually converge at a point.<br>(C) appear to meet when they are produced in the backward direction.<br>(D) appear to diverge from a point.<br>Which of the above statements are correct?",
+    options: { A: "(A) and (D)", B: "(B) and (D)", C: "(A) and (B)", D: "(B) and (C)" },
+    answer: "C", set: "", year: "2020"
+  },
+  {
+    question: "Consider the following properties of virtual images :<br>(A) cannot be projected on the screen<br>(B) are formed by both concave and convex lens<br>(C) are always erect<br>(D) are always inverted<br>The correct properties are :",
+    options: { A: "(A) and (D)", B: "(A) and (B)", C: "(A), (B) and (C)", D: "(A), (B) and (D)" },
+    answer: "C", set: "", year: "2020"
+  },
+  {
+    question: "When an object is kept within the focus of a concave mirror, an enlarged image is formed behind the mirror. This image is:",
+    options: { A: "real", B: "inverted", C: "virtual and inverted", D: "virtual and erect" },
+    answer: "D", set: "", year: "2020"
+  }
 ];
