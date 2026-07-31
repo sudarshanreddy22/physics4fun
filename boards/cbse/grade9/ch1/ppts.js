8f396bd78bcd3ca86bf14e33111fe72e8284d675
sudarshanreddy22 <sudarshanece22@gmail.com>
2026-07-31 13:29:19 +0530

    Update ppts.js

--- boards/cbse/grade9/ch1/ppts.js
+++ boards/cbse/grade9/ch1/ppts.js
@@ -1,5 +1,5 @@
 /* PPTs - Motion | Grade 9 */
 window.pptsData = [
-  { title: "Motion Basics", slides: 12, icon: "🏃", url: "https://canva.link/kr8m8e9mkeqckgx" },
+  { title: "Desribing Motion Around Us", slides: 12, icon: "🏃", url: "https://canva.link/kr8m8e9mkeqckgx" },
   { title: "Equations of Motion", slides: 10, icon: "📐", url: "#" }
 ];
