c03477b28004be181ff61dac2421e8c096b5b7f8
sudarshanreddy22 <sudarshanece22@gmail.com>
2026-08-02 21:42:57 +0530

    Add files via upload

--- boards/cbse/grade9/ch1/ppts.js
+++ boards/cbse/grade9/ch1/ppts.js
@@ -1,5 +1,5 @@
 /* PPTs - Motion | Grade 9 */
 window.pptsData = [
-  { title: "Desribing Motion Around Us", slides: 12, icon: "🏃", url: "https://canva.link/kr8m8e9mkeqckgx" },
-  { title: "Equations of Motion", slides: 10, icon: "📐", url: "#" }
+  { title: "Describing Motion Around Us", slides: 12, icon: "🏃", url: "https://canva.link/kr8m8e9mkeqckgx" },
+  { title: "Revise, Reflect, Refine", slides: 10, icon: "📐", url: "https://canva.link/2j5yw3g9u72tc8n" }
 ];
